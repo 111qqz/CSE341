dc6896a0a1df4ea1c3285ab50ba8794d55110a9b
renkuanze <renkuanze@sensetime.com>
2019-09-17 00:46:23 +0800

    finish first 2 problems

--- hw4/hw4.rkt
+++ hw4/hw4.rkt
@@ -5,3 +5,15 @@
 
 ;; put your code below
 
+(define (sequence low high stride)
+    (if(<= low high)
+       (cons low (sequence (+ low stride) high stride))
+       null))
+
+(define (string-append-map xs suffix)
+  (map (lambda (x)
+         (string-append x suffix))
+       xs))
+  
+
+
